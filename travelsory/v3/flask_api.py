74fba055d9b5a036261f4d4e324596f28d06f35f
Suman Banerjee <iamsuman@gmail.com>
2020-09-28 10:14:06 -0700

    v3 files

--- travelsory/v3/flask_api.py
+++ travelsory/v3/flask_api.py
@@ -1,12 +1,20 @@
 import flask
+from travelsory.v3 import lookup
+
 
 app = flask.Flask(__name__)
-#app.config["DEBUG"] = True
+# app.config["DEBUG"] = True
+
 
 
 @app.route("/health", methods=["GET"])
-def home():
+def health():
+    return "{\"code\":200,\"status\":\"ok\"}"
+
+@app.route("/diag", methods=["GET"])
+def diag():
     return "{\"code\":200,\"status\":\"ok\"}"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+
+    app.run(host='0.0.0.0', port=5000)
---
+++ travelsory/v3/lookup.py
@@ -0,0 +1,64 @@
+import argparse
+import json
+import sys
+
+
+class Core:
+    # def ___init__(self):
+
+    def parse_cli(self):
+        parser = argparse.ArgumentParser(
+            description="Program to lookup countrycode",
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            epilog="Doc: https://github.com/iamsuman/travel-advisory")
+        parser.add_argument("-c", "--countryCode", help="countryCode", required=True, default="")
+        args = parser.parse_args()
+        return args
+
+    def validate_args(self, country_code):
+        if country_code.isalpha():
+            return "alpha"
+        else:
+            try:
+                # country_code = '{"countryCode":["AU", "AD"]}'
+                out = json.loads(country_code)
+                if out.get("countryCode"):
+                    if isinstance(out.get("countryCode"), list):
+                        return "json"
+            except:
+                sys.exit("Not a valid input, need json format like \'{\"countryCode\":[\"AU\", \"AD\"]}\'")
+
+    def load_travel_data(self, file_name):
+        with open("data.json", "r") as json_file:
+            country_dict = json.load(json_file)
+        data = country_dict["data"]
+        return data
+
+    def get_country(self, data, country_code):
+        if data.get(country_code):
+            return data[country_code]["name"]
+        else:
+            return "404: Country Code does not exist"
+
+    def execute(self, args):
+        data = self.load_travel_data("data.json")
+        input_type = self.validate_args(args.countryCode)
+        if input_type == "alpha":
+            country = self.get_country(data, args.countryCode)
+            print(country)
+        elif input_type == "json":
+            input_dict = json.loads(args.countryCode)
+            result_json = {}
+            for country_code in input_dict["countryCode"]:
+                result_json[country_code] = self.get_country(data, country_code)
+            print(result_json)
+
+
+def main():
+    core = Core()
+    args = core.parse_cli()
+    core.execute(args)
+
+
+if __name__ == "__main__":
+    main()
